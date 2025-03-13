178992eb52eaa8f63d1051ab7ded4c9110f1113c
Tmazst <55834770+Tmazst@users.noreply.github.com>
2025-03-13 15:03:42 +0200

    130325

--- Forms.py
+++ Forms.py
@@ -27,7 +27,7 @@ class Register(FlaskForm):
 
 class JobPostForm(FlaskForm):
     advert_image = FileField("Upload Advert Image")
-    details = TextAreaField('Additional Info', validators=[Optional()])
+    details = TextAreaField('Additional Info', validators=[Optional(),Length(max=64)])
     deadline = DateField('Deadline',validators=[Optional()])
     link = URLField('Paste Link here',validators=[DataRequired(),Length(min=8, max=64)])
     publish = SubmitField('Post')
