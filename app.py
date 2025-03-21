cebe604671d16eb1b293079aa3f70b3590c00609
Tmazst <55834770+Tmazst@users.noreply.github.com>
2025-03-21 12:28:31 +0200

    210325

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ if os.environ.get('ENV') == 'LOCAL':
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tht_db.db"
 else:#Online
     app.config[
-    "SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://techtlnf_tmaz:!Tmazst41#@localhost/techt_lnf_tht_db"
+    "SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://techtlnf_tmaz:!Tmazst41#@localhost/techtlnf_tht_db"
 
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 280}
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
