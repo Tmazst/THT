d13afb6407f4af32b7588c06acc21ef69127e00b
Tmazst <55834770+Tmazst@users.noreply.github.com>
2025-04-04 23:41:36 +0200

    040425

--- app.py
+++ app.py
@@ -340,14 +340,14 @@ def updates_modal():
         if ifip.latest_visit:
             days_missed = datetime.now() - ifip.latest_visit
             print("Your last Vist: ",days_missed.days)
-            missed__posts = jobs_posted.query.filter(jobs_posted.timepstamp > ifip.latest_visit).all()
+            missed__posts = jobs_posted.query.all() #filter(jobs_posted.timepstamp > ifip.latest_visit).
             print("Missed: ", missed__posts)
             missed_posts = missed__posts
         else:
             days_missed = datetime.now() - ifip.timestamp
             print("Your last Vist2: ",days_missed.days)
             # Fetch posts that were created after the user's first visit (timestamp)
-            missed__posts = jobs_posted.query.filter(jobs_posted.timepstamp > ifip.timestamp).all()
+            missed__posts = jobs_posted.query.all() #.filter(jobs_posted.timepstamp > ifip.timestamp)
             print("Missed: ", missed__posts)
             missed_posts = missed__posts
 
